cfeb19896d93c4531f7d96267a4ff22f55385286
Tommy Wu <wtommy@google.com>
2026-04-11 01:05:36 +0000

    Revert 'port virtio_disk to virtio spec 1.0+' for gem5 compatibility
    Revert Commit : cd00a82

--- kernel/memlayout.h
+++ kernel/memlayout.h
@@ -22,8 +22,8 @@
 #define UART0_IRQ 10
 
 // virtio mmio interface
-#define VIRTIO0 0x10001000
-#define VIRTIO0_IRQ 1
+#define VIRTIO0 0x10008000
+#define VIRTIO0_IRQ 8
 
 // core local interruptor (CLINT), which contains the timer.
 #define CLINT 0x2000000L
